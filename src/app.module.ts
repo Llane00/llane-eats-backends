5094cacc8d9a36a773baee88df00d1efa01531b2
Llane00 <bluetinestyle@126.com>
2022-02-04 22:34:55 +0800

    feat: use TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'llane',
+      password: '12345',
+      database: 'llane-eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
-    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
